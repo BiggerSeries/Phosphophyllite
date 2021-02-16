7e22aa9e82b66091eed02178f766e96963f7d735
RogueLogix <roguelogix@roguelogix.net>
2021-02-16 11:42:47 -0800

    add common blockstates class

---
+++ src/main/java/net/roguelogix/phosphophyllite/util/BlockStates.java
@@ -0,0 +1,8 @@
+package net.roguelogix.phosphophyllite.util;
+
+import net.minecraft.state.BooleanProperty;
+
+public class BlockStates {
+    public static final BooleanProperty PORT_DIRECTION = BooleanProperty.create("port_direction");
+    public static final BooleanProperty ACTIVITY = BooleanProperty.create("active");
+}
