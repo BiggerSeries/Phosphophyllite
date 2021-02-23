487d59893dc70edae9ce2fb9144115abf105201a
RogueLogix <roguelogix@roguelogix.net>
2021-02-22 17:26:24 -0800

    pass directions to DirectionProperty creation

--- src/main/java/net/roguelogix/phosphophyllite/util/BlockStates.java
+++ src/main/java/net/roguelogix/phosphophyllite/util/BlockStates.java
@@ -2,9 +2,10 @@ package net.roguelogix.phosphophyllite.util;
 
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.DirectionProperty;
+import net.minecraft.util.Direction;
 
 public class BlockStates {
     public static final BooleanProperty PORT_DIRECTION = BooleanProperty.create("port_direction");
     public static final BooleanProperty ACTIVITY = BooleanProperty.create("active");
-    public static final DirectionProperty FACING = DirectionProperty.create("facing");
+    public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.values());
 }
