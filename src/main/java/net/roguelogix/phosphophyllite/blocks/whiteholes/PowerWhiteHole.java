163e95face1e4af9450f1af0d6f432d57414cf86
RogueLogix <roguelogix@roguelogix.net>
2021-07-23 23:11:22 -0700

    fix power white hole ticking

--- src/main/java/net/roguelogix/phosphophyllite/blocks/whiteholes/PowerWhiteHole.java
+++ src/main/java/net/roguelogix/phosphophyllite/blocks/whiteholes/PowerWhiteHole.java
@@ -3,9 +3,12 @@ package net.roguelogix.phosphophyllite.blocks.whiteholes;
 
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
 import net.roguelogix.phosphophyllite.registry.RegisterBlock;
@@ -26,6 +29,15 @@ public class PowerWhiteHole extends Block implements EntityBlock {
     
     public PowerWhiteHole() {
         super(Properties.of(Material.METAL));
+    }
+    
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level p_153212_, BlockState p_153213_, BlockEntityType<T> p_153214_) {
+        return (level, pos, state, entity) -> {
+            assert entity instanceof PowerWhiteHoleTile;
+            ((PowerWhiteHoleTile) entity).tick();
+        };
     }
     
     @Nullable
