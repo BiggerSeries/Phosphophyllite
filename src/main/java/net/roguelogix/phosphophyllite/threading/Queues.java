ab16c45516eac03b7f65e285a15cee3d55d1972e
RogueLogix <roguelogix@roguelogix.net>
2021-10-01 13:17:28 -0700

    init queues on class load, and ensure they will load without forge

--- src/main/java/net/roguelogix/phosphophyllite/threading/Queues.java
+++ src/main/java/net/roguelogix/phosphophyllite/threading/Queues.java
@@ -5,16 +5,27 @@ import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.roguelogix.phosphophyllite.registry.OnModLoad;
 
 public class Queues {
-    public static final WorkQueue serverThread = new WorkQueue();
-    public static final WorkQueue offThread = new WorkQueue();
+    public static final WorkQueue serverThread;
+    public static final WorkQueue offThread;
     
     @OnModLoad
     private static void onModLoad() {
+    }
+    
+    static {
+        WorkQueue serverThread1 = null;
         int threads = Runtime.getRuntime().availableProcessors();
         threads = Math.max(1, threads - 1); // if possible, leave a core for the main server threads
-        if (FMLEnvironment.dist == Dist.CLIENT) {
-            threads = Math.max(1, threads - 1); // if possible, leave a core for the main client thread too
+        try {
+            if (FMLEnvironment.dist == Dist.CLIENT) {
+                threads = Math.max(1, threads - 1); // if possible, leave a core for the main client thread too
+            }
+            serverThread1 = new WorkQueue();
+        }catch (NoClassDefFoundError ignored){
+            // happens when forge isn't loaded
         }
+        serverThread = serverThread1;
+        offThread = new WorkQueue();
         offThread.addProcessingThreads(threads, "Phosphophyllite OffThread Queue Worker Thread #");
     }
 }
