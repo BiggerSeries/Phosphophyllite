faa414316b945921bde37840c34efda216842ed8
RogueLogix <roguelogix@roguelogix.net>
2021-12-01 22:13:22 -0800

    Re-enable mixins

--- src/main/java/net/roguelogix/phosphophyllite/mixin/LevelRendererMixin.java
+++ src/main/java/net/roguelogix/phosphophyllite/mixin/LevelRendererMixin.java
@@ -1,28 +1,28 @@
-//package net.roguelogix.phosphophyllite.mixin;
-//
-//import com.mojang.blaze3d.vertex.PoseStack;
-//import com.mojang.math.Matrix4f;
-//import net.minecraft.client.Camera;
-//import net.minecraft.client.renderer.GameRenderer;
-//import net.minecraft.client.renderer.LevelRenderer;
-//import net.minecraft.client.renderer.LightTexture;
-//import net.roguelogix.phosphophyllite.mixinhelpers.LevelRenderStages;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//
-//@Mixin(LevelRenderer.class)
-//public class LevelRendererMixin {
-//    @Inject(
-//            method = "renderLevel",
-//            at = @At(
-//                    value = "INVOKE",
-//                    target = "Lnet/minecraft/client/renderer/LevelRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLcom/mojang/math/Matrix4f;)V",
-//                    ordinal = 0
-//            )
-//    )
-//    public void renderFirst(PoseStack pMatrixStack, float pPartialTicks, long pFinishTimeNano, boolean pDrawBlockOutline, Camera pActiveRenderInfo, GameRenderer pGameRenderer, LightTexture pLightmap, Matrix4f pProjection, CallbackInfo ci) {
-//        LevelRenderStages.renderFirst(pMatrixStack, pPartialTicks, pFinishTimeNano, pDrawBlockOutline, pActiveRenderInfo, pGameRenderer, pLightmap, pProjection);
-//    }
-//}
+package net.roguelogix.phosphophyllite.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Matrix4f;
+import net.minecraft.client.Camera;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
+import net.roguelogix.phosphophyllite.mixinhelpers.LevelRenderStages;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LevelRenderer.class)
+public class LevelRendererMixin {
+    @Inject(
+            method = "renderLevel",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/renderer/LevelRenderer;renderChunkLayer(Lnet/minecraft/client/renderer/RenderType;Lcom/mojang/blaze3d/vertex/PoseStack;DDDLcom/mojang/math/Matrix4f;)V",
+                    ordinal = 0
+            )
+    )
+    public void renderFirst(PoseStack pMatrixStack, float pPartialTicks, long pFinishTimeNano, boolean pDrawBlockOutline, Camera pActiveRenderInfo, GameRenderer pGameRenderer, LightTexture pLightmap, Matrix4f pProjection, CallbackInfo ci) {
+        LevelRenderStages.renderFirst(pMatrixStack, pPartialTicks, pFinishTimeNano, pDrawBlockOutline, pActiveRenderInfo, pGameRenderer, pLightmap, pProjection);
+    }
+}
--- src/main/java/net/roguelogix/phosphophyllite/mixin/MinecraftShutdownMixin.java
+++ src/main/java/net/roguelogix/phosphophyllite/mixin/MinecraftShutdownMixin.java
@@ -1,18 +1,18 @@
-//package net.roguelogix.phosphophyllite.mixin;
-//
-//import net.minecraft.client.Minecraft;
-//import net.roguelogix.phosphophyllite.quartz.internal.QuartzCore;
-//import org.spongepowered.asm.mixin.Mixin;
-//import org.spongepowered.asm.mixin.injection.At;
-//import org.spongepowered.asm.mixin.injection.Inject;
-//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-//
-//@Mixin(Minecraft.class)
-//public class MinecraftShutdownMixin {
-//    @Inject(method = "stop", at = @At("HEAD"))
-//    public void stop(CallbackInfo ci) {
-//        if (Minecraft.getInstance().isRunning()) {
-//            QuartzCore.shutdown();
-//        }
-//    }
-//}
+package net.roguelogix.phosphophyllite.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.roguelogix.phosphophyllite.quartz.internal.QuartzCore;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MinecraftShutdownMixin {
+    @Inject(method = "stop", at = @At("HEAD"))
+    public void stop(CallbackInfo ci) {
+        if (Minecraft.getInstance().isRunning()) {
+            QuartzCore.shutdown();
+        }
+    }
+}
