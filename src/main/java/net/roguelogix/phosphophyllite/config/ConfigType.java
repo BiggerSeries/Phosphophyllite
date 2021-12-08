95d1ae73e633dc1787a02293a92e63d929fc2809
RogueLogix <roguelogix@roguelogix.net>
2021-12-07 22:10:38 -0800

    have config type be side aware

--- src/main/java/net/roguelogix/phosphophyllite/config/ConfigType.java
+++ src/main/java/net/roguelogix/phosphophyllite/config/ConfigType.java
@@ -1,7 +1,15 @@
 package net.roguelogix.phosphophyllite.config;
 
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
 public enum ConfigType {
-    CLIENT,
-    COMMON,
-    SERVER
+    CLIENT(FMLEnvironment.dist.isClient()),
+    COMMON(true),
+    SERVER(true);
+    
+    public final boolean appliesToPhysicalSide;
+    
+    ConfigType(boolean appliesToPhysicalSide) {
+        this.appliesToPhysicalSide = appliesToPhysicalSide;
+    }
 }
