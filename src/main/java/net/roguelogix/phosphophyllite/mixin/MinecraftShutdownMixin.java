a787af8c98d18bf7c35cf9bad24ce442d97d2a4f
RogueLogix <roguelogix@roguelogix.net>
2021-12-14 21:12:35 -0800

    move shutdown hook further into shutdown
    ensures that another frame wont be drawn after shutdown starts

--- src/main/java/net/roguelogix/phosphophyllite/mixin/MinecraftShutdownMixin.java
+++ src/main/java/net/roguelogix/phosphophyllite/mixin/MinecraftShutdownMixin.java
@@ -9,10 +9,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
 public class MinecraftShutdownMixin {
-    @Inject(method = "stop", at = @At("HEAD"))
+    @Inject(method = "close", at = @At("HEAD"))
     public void stop(CallbackInfo ci) {
-        if (Minecraft.getInstance().isRunning()) {
-            QuartzCore.shutdown();
-        }
+        QuartzCore.shutdown();
     }
 }
