ec4fdb1bf8dbe7828cdd58c79374ff6a810130ae
RogueLogix <roguelogix@roguelogix.net>
2021-12-26 13:25:46 -0700

    register quartz config earlier
    quartz needs to read this before mods are initialized

--- src/main/java/net/roguelogix/phosphophyllite/quartz/QuartzConfig.java
+++ src/main/java/net/roguelogix/phosphophyllite/quartz/QuartzConfig.java
@@ -1,16 +1,31 @@
 package net.roguelogix.phosphophyllite.quartz;
 
 import net.roguelogix.phosphophyllite.Phosphophyllite;
+import net.roguelogix.phosphophyllite.config.ConfigManager;
 import net.roguelogix.phosphophyllite.config.ConfigType;
 import net.roguelogix.phosphophyllite.config.ConfigValue;
+import net.roguelogix.phosphophyllite.quartz.internal.QuartzCore;
 import net.roguelogix.phosphophyllite.quartz.internal.gl.GLConfig;
 import net.roguelogix.phosphophyllite.quartz.internal.vk.VKConfig;
+import net.roguelogix.phosphophyllite.registry.IgnoreRegistration;
+import net.roguelogix.phosphophyllite.registry.OnModLoad;
 import net.roguelogix.phosphophyllite.registry.RegisterConfig;
 
 public class QuartzConfig {
-    
+
+    @IgnoreRegistration
     @RegisterConfig(folder = Phosphophyllite.modid, name = "quartz", type = ConfigType.CLIENT)
     public static final QuartzConfig INSTANCE = new QuartzConfig();
+    
+    static {
+        try {
+            // this needs to be registered extra extra early, so it can be read at quartz init
+            ConfigManager.registerConfig(QuartzConfig.class.getField("INSTANCE"), Phosphophyllite.modid);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            throw new IllegalStateException();
+        }
+    }
     
     @ConfigValue(hidden = true, enableAdvanced = true)
     public final boolean enableAdvanced;
