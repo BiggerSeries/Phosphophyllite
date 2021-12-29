a4afc9c066beb07a012b04e0c1879e77c5915f74
RogueLogix <roguelogix@roguelogix.net>
2021-12-29 14:37:56 -0700

    use reflection backup if AT fails
    AT loading can fail in some circumstances, and cause a weird crash
    none of those cases seem to be normal runtime, so, falling back to reflection should be fine, and is better than skipping it entirely

--- src/main/java/net/roguelogix/phosphophyllite/quartz/internal/common/B3DStateHelper.java
+++ src/main/java/net/roguelogix/phosphophyllite/quartz/internal/common/B3DStateHelper.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.BufferUploader;
 import net.roguelogix.phosphophyllite.util.MethodsReturnNonnullByDefault;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.lang.reflect.Field;
 
 import static org.lwjgl.opengl.GL33C.*;
 
@@ -13,19 +14,70 @@ import static org.lwjgl.opengl.GL33C.*;
 @ParametersAreNonnullByDefault
 @MethodsReturnNonnullByDefault
 public class B3DStateHelper {
+    
+    private static final Field lastVertexBufferObjectField;
+    private static final Field lastIndexBufferObjectField;
+    private static final Field lastVertexArrayObjectField;
+    private static final Field lastFormatField;
+    
+    static {
+        Field lastVertexBufferObjectField1;
+        Field lastIndexBufferObjectField1;
+        Field lastVertexArrayObjectField1;
+        Field lastFormatField1;
+        try {
+            lastVertexBufferObjectField1 = BufferUploader.class.getDeclaredField("lastVertexBufferObject");
+            lastIndexBufferObjectField1 = BufferUploader.class.getDeclaredField("lastIndexBufferObject");
+            lastVertexArrayObjectField1 = BufferUploader.class.getDeclaredField("lastVertexArrayObject");
+            lastFormatField1 = BufferUploader.class.getDeclaredField("lastFormat");
+        } catch (NoSuchFieldException e) {
+            lastVertexBufferObjectField1 = null;
+            lastIndexBufferObjectField1 = null;
+            lastVertexArrayObjectField1 = null;
+            lastFormatField1 = null;
+            e.printStackTrace();
+        }
+        lastVertexBufferObjectField = lastVertexBufferObjectField1;
+        lastIndexBufferObjectField = lastIndexBufferObjectField1;
+        lastVertexArrayObjectField = lastVertexArrayObjectField1;
+        lastFormatField = lastFormatField1;
+    }
+    
     public static void bindArrayBuffer(int buffer) {
         glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        BufferUploader.lastVertexBufferObject = 0;
+        try {
+            BufferUploader.lastVertexBufferObject = 0;
+        } catch (IllegalAccessError e) {
+            try {
+                lastVertexBufferObjectField.set(null, 0);
+            } catch (IllegalAccessException ignored) {
+            }
+        }
     }
     
     public static void bindElementBuffer(int buffer) {
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer);
-        BufferUploader.lastIndexBufferObject = 0;
+        try {
+            BufferUploader.lastIndexBufferObject = 0;
+        } catch (IllegalAccessError e) {
+            try {
+                lastIndexBufferObjectField.set(null, 0);
+            } catch (IllegalAccessException ignored) {
+            }
+        }
     }
     
     public static void bindVertexArray(int vertexArray) {
         glBindVertexArray(vertexArray);
-        BufferUploader.lastVertexArrayObject = 0;
-        BufferUploader.lastFormat = null;
+        try {
+            BufferUploader.lastVertexArrayObject = 0;
+            BufferUploader.lastFormat = null;
+        } catch (IllegalAccessError e) {
+            try {
+                lastVertexArrayObjectField.set(null, 0);
+                lastFormatField.set(null, null);
+            } catch (IllegalAccessException ignored) {
+            }
+        }
     }
 }
