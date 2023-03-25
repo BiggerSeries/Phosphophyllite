9c4eea4cb1971fe91124f6614e5abcbc83574dca
RogueLogix <roguelogix@roguelogix.net>
2023-03-25 10:10:31 -0700

    Don't construct multiblock when too many parts are found
    or too few, related BiggerSeries/BiggerReactors#71

--- src/main/java/net/roguelogix/phosphophyllite/multiblock2/rectangular/IRectangularMultiblock.java
+++ src/main/java/net/roguelogix/phosphophyllite/multiblock2/rectangular/IRectangularMultiblock.java
@@ -69,6 +69,7 @@ public interface IRectangularMultiblock<
         
         private boolean cornerSpecificValidation;
         private boolean frameSpecificValidation;
+        private int foundMultiblockBlocks;
         
         @OnModLoad
         public static void register() {
@@ -153,7 +154,11 @@ public interface IRectangularMultiblock<
             controller.rectangularValidationStarted();
             cornerSpecificValidation = controller.cornerSpecificValidation();
             frameSpecificValidation = controller.frameSpecificValidation();
+            foundMultiblockBlocks = 0;
             Util.chunkCachedBlockStateIteration(controller.min(), controller.max(), controller.level, this::blockValidation);
+            if (foundMultiblockBlocks != controller.blocks.size()) {
+                throw new ValidationException(Component.translatable("multiblock.error.phosphophyllite.mismatched_block_count", foundMultiblockBlocks, controller.blocks.size()));
+            }
         }
         
         private void blockValidation(BlockState blockState, Vector3ic pos) throws ValidationException {
@@ -177,13 +182,13 @@ public interface IRectangularMultiblock<
             if (pos.z() == minZ || pos.z() == maxZ) {
                 extremes++;
             }
+            // use of old switch for case to case rolling is intentional
             switch (extremes) {
                 case 3: {
                     if (cornerSpecificValidation) {
                         if (block instanceof IRectangularMultiblockBlock && controller.blockTypeValidator.test(block)) {
-                            if (!((IRectangularMultiblockBlock) block).isGoodForCorner()) {
-                                throw new InvalidBlock(block, pos, "corner");
-                            } else {
+                            if (((IRectangularMultiblockBlock) block).isGoodForCorner()) {
+                                foundMultiblockBlocks++;
                                 break;
                             }
                         }
@@ -193,9 +198,8 @@ public interface IRectangularMultiblock<
                 case 2: {
                     if (frameSpecificValidation) {
                         if (block instanceof IRectangularMultiblockBlock && controller.blockTypeValidator.test(block)) {
-                            if (!((IRectangularMultiblockBlock) block).isGoodForFrame()) {
-                                throw new InvalidBlock(block, pos, "frame");
-                            } else {
+                            if (((IRectangularMultiblockBlock) block).isGoodForFrame()) {
+                                foundMultiblockBlocks++;
                                 break;
                             }
                         }
@@ -204,9 +208,8 @@ public interface IRectangularMultiblock<
                 }
                 case 1: {
                     if (block instanceof IRectangularMultiblockBlock && controller.blockTypeValidator.test(block)) {
-                        if (!((IRectangularMultiblockBlock) block).isGoodForExterior()) {
-                            throw new InvalidBlock(block, pos, "exterior");
-                        } else {
+                        if (((IRectangularMultiblockBlock) block).isGoodForExterior()) {
+                            foundMultiblockBlocks++;
                             break;
                         }
                     }
@@ -214,11 +217,11 @@ public interface IRectangularMultiblock<
                 }
                 default: {
                     if (block instanceof IRectangularMultiblockBlock && controller.blockTypeValidator.test(block)) {
-                        if (!((IRectangularMultiblockBlock) block).isGoodForInterior()) {
-                            throw new InvalidBlock(block, pos, "interior");
-                        } else {
+                        if (((IRectangularMultiblockBlock) block).isGoodForInterior()) {
+                            foundMultiblockBlocks++;
                             break;
                         }
+                        throw new InvalidBlock(block, pos, "interior");
                     }
                     
                     if (!controller.allowedInteriorBlock(block)) {
