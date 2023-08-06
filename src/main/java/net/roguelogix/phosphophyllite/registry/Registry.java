dba020e7170b01437c2dca0e66b0213b671b41b5
Eric <gizmocodes@users.noreply.github.com>
2023-08-06 17:18:53 -0500

    Remove broken oregen registration system

--- src/main/java/net/roguelogix/phosphophyllite/registry/Registry.java
+++ src/main/java/net/roguelogix/phosphophyllite/registry/Registry.java
@@ -2,8 +2,6 @@ package net.roguelogix.phosphophyllite.registry;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
-import it.unimi.dsi.fastutil.objects.ReferenceArrayList;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -18,10 +16,8 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
-import net.minecraftforge.common.CreativeModeTabRegistry;
 import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
 import net.minecraftforge.common.extensions.IForgeMenuType;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fluids.FluidType;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
@@ -100,9 +96,6 @@ public class Registry {
     
     private final WorkQueue commonSetupQueue = new WorkQueue();
     private FMLCommonSetupEvent commonSetupEvent;
-
-//    private final ArrayList<Runnable> biomeLoadingEventHandlers = new ArrayList<>();
-//    private BiomeLoadingEvent biomeLoadingEvent;
     
     private final Map<String, AnnotationHandler> annotationMap = new Object2ObjectOpenHashMap<>();
     
@@ -113,7 +106,6 @@ public class Registry {
         annotationMap.put(RegisterContainer.class.getName(), this::registerContainerAnnotation);
         annotationMap.put(RegisterTile.class.getName(), this::registerTileAnnotation);
         annotationMap.put(RegisterCapability.class.getName(), this::registerCapabilityAnnotation);
-//        annotationMap.put(RegisterOre.class.getName(), this::registerWorldGenAnnotation);
     }
     
     public Registry(@Nonnull List<ResourceLocation> tabsBefore, @Nonnull List<ResourceLocation> tabsAfter) {
@@ -162,7 +154,6 @@ public class Registry {
         
         ModBus.addListener(this::registerEvent);
 
-//        MinecraftForge.EVENT_BUS.addListener(EventPriority.HIGH, this::biomeLoadingEventHandler);
         ModBus.addListener(this::commonSetupEventHandler);
         
         if (FMLEnvironment.dist == Dist.CLIENT) {
@@ -297,12 +288,6 @@ public class Registry {
         commonSetupQueue.runAll();
         commonSetupEvent = null;
     }
-
-//    private void biomeLoadingEventHandler(BiomeLoadingEvent event) {
-//        biomeLoadingEvent = event;
-//        biomeLoadingEventHandlers.forEach(Runnable::run);
-//        biomeLoadingEvent = null;
-//    }
     
     private interface AnnotationHandler {
         void run(final String modNamespace, final Class<?> clazz, final String memberName);
@@ -865,86 +850,6 @@ public class Registry {
             LOGGER.error("Failed to register capability field " + memberName + " in " + declaringClass.getSimpleName());
         }
     }
-
-//    private void registerWorldGenAnnotation(String modNamespace, Class<?> oreClazz, final String memberName) {
-//        commonSetupQueue.enqueue(() -> {
-//            final Block oreInstance;
-//            try {
-//                final Field field = oreClazz.getDeclaredField(memberName);
-//                if (field.isAnnotationPresent(IgnoreRegistration.class)) {
-//                    return;
-//                }
-//                field.setAccessible(true);
-//                oreInstance = (Block) field.get(null);
-//            } catch (NoSuchFieldException e) {
-//                LOGGER.error("Unable to find block field for block " + memberName);
-//                return;
-//            } catch (IllegalAccessException e) {
-//                LOGGER.error("Unable to access block field for block " + memberName);
-//                return;
-//            }
-//
-//            final ResourceLocation resourceLocation = ForgeRegistries.BLOCKS.getKey(oreInstance);
-//            assert resourceLocation != null;
-//
-//            if (!(oreInstance instanceof IPhosphophylliteOre oreInfo)) {
-//                LOGGER.error("Attempt to register non-IPhosphophylliteOre block for world generation");
-//                return;
-//            }
-//
-//            final var targetBlockStates = new ArrayList<OreConfiguration.TargetBlockState>();
-//
-//            final var blockstate = oreInstance.defaultBlockState();
-//
-//            if (oreInfo.isNetherOre()) {
-//                targetBlockStates.add(OreConfiguration.target(OreFeatures.NETHER_ORE_REPLACEABLES, blockstate));
-//            } else {
-//                final var stoneVariant = oreInfo.stoneVariant();
-//                if(stoneVariant != null){
-//                    targetBlockStates.add(OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, stoneVariant.defaultBlockState()));
-//                }
-//                final var deepslateVariant = oreInfo.deepslateVariant();
-//                if (deepslateVariant != null) {
-//                    targetBlockStates.add(OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, deepslateVariant.defaultBlockState()));
-//                }
-//            }
-//
-//            final var oreConfiguration = new OreConfiguration(ImmutableList.copyOf(targetBlockStates), oreInfo.size());
-//
-//            final ConfiguredFeature<?, ?> configuredFeature = new ConfiguredFeature<>(Feature.ORE, oreConfiguration);
-//
-//            final HashSet<String> spawnBiomes = new HashSet<>(Arrays.asList(oreInfo.spawnBiomes()));
-//            commonSetupEvent.enqueueWork(() -> net.minecraft.core.Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, resourceLocation, configuredFeature));
-//
-//            final var placementModifiers = new ArrayList<PlacementModifier>();
-//            if (!oreInfo.doSpawn()) {
-//                placementModifiers.add(BlockPredicateFilter.forPredicate(BlockPredicate.not(BlockPredicate.alwaysTrue())));
-//            }
-//            final var rangeLower = oreInfo.minLevel() == Integer.MIN_VALUE ? VerticalAnchor.BOTTOM : VerticalAnchor.absolute(oreInfo.minLevel());
-//            placementModifiers.addAll(OrePlacements.orePlacement(
-//                    CountPlacement.of(oreInfo.count()),
-//                    HeightRangePlacement.uniform(rangeLower, VerticalAnchor.absolute(oreInfo.maxLevel()))
-//            ));
-//
-//            final var placedFeature = new PlacedFeature(new Holder.Direct<>(configuredFeature), ImmutableList.copyOf(placementModifiers));
-//            final var placedFeatureHolder = new Holder.Direct<>(placedFeature);
-//
-//            commonSetupEvent.enqueueWork(() -> net.minecraft.core.Registry.register(BuiltinRegistries.PLACED_FEATURE, resourceLocation, placedFeature));
-//
-//            biomeLoadingEventHandlers.add(() -> {
-//                if ((biomeLoadingEvent.getCategory() == Biome.BiomeCategory.NETHER) != oreInfo.isNetherOre()) {
-//                    return;
-//                }
-//                if (spawnBiomes.size() > 0) {
-//                    if (!spawnBiomes.contains(Objects.requireNonNull(biomeLoadingEvent.getName()).toString())) {
-//                        return;
-//                    }
-//                }
-//
-//                biomeLoadingEvent.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(placedFeatureHolder);
-//            });
-//        });
-//    }
     
     private void registerConfigAnnotation(String modNamespace, Class<?> configClazz, final String memberName) {
         try {
