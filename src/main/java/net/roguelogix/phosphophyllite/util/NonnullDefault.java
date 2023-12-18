11f72110d606b9bb95cf9ffdec35a19aba4d2be7
RogueLogix <roguelogix@roguelogix.net>
2023-12-18 11:23:28 -0800

    add nonnull targets

--- src/main/java/net/roguelogix/phosphophyllite/util/NonnullDefault.java
+++ src/main/java/net/roguelogix/phosphophyllite/util/NonnullDefault.java
@@ -8,7 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @NotNull
-@TypeQualifierDefault({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@TypeQualifierDefault({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.TYPE_PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.RECORD_COMPONENT})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NonnullDefault {
     // so i don't rely on any minecraft/mojang code, but can still use this
