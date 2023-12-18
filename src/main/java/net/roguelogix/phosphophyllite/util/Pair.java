8e6adc67c657f2b4b0a3b1cb6314155554ea52b0
RogueLogix <roguelogix@roguelogix.net>
2023-12-18 11:23:15 -0800

    change pair to be a record

--- src/main/java/net/roguelogix/phosphophyllite/util/Pair.java
+++ src/main/java/net/roguelogix/phosphophyllite/util/Pair.java
@@ -1,12 +1,5 @@
 package net.roguelogix.phosphophyllite.util;
 
 
-public class Pair<F, S> {
-    public final F first;
-    public final S second;
-    
-    public Pair(final F first, final S second) {
-        this.first = first;
-        this.second = second;
-    }
+public record Pair<F, S>(F first, S second) {
 }
