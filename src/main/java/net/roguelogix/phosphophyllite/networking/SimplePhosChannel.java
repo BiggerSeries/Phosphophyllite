d1b37c8d53a79a897de0c219bc12411ea57d809a
RogueLogix <roguelogix@roguelogix.net>
2024-02-18 12:56:19 -0800

    actually register network packets, and allow null handlers

--- src/main/java/net/roguelogix/phosphophyllite/networking/SimplePhosChannel.java
+++ src/main/java/net/roguelogix/phosphophyllite/networking/SimplePhosChannel.java
@@ -6,16 +6,21 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.fml.ModLoader;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlerEvent;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.neoforged.neoforge.network.handling.IPayloadHandler;
 import net.roguelogix.phosphophyllite.Phosphophyllite;
+import net.roguelogix.phosphophyllite.registry.OnModLoad;
 import net.roguelogix.phosphophyllite.robn.ROBN;
 import net.roguelogix.phosphophyllite.serialization.PhosphophylliteCompound;
 import net.roguelogix.phosphophyllite.util.NonnullDefault;
 import net.roguelogix.phosphophyllite.util.Pair;
 
+import javax.annotation.Nullable;
 import java.util.List;
 import java.util.function.BiConsumer;
 
@@ -26,15 +31,22 @@ public class SimplePhosChannel {
     public static void register(final RegisterPayloadHandlerEvent event) {
         for (var pair : channelsToRegister) {
             FriendlyByteBuf.Reader<CompoundPacket> reader = (buf) -> new CompoundPacket((PhosphophylliteCompound) ROBN.fromROBN(ByteArrayList.wrap(buf.readByteArray())), pair.first());
-            event.registrar(Phosphophyllite.modid).common(pair.first(), reader, pair.second());
+            event.registrar(pair.first().getNamespace()).common(pair.first(), reader, pair.second());
         }
+    }
+    
+    @OnModLoad
+    private static void onModLoad() {
+        ModLoadingContext.get().getActiveContainer().getEventBus().addListener(SimplePhosChannel::register);
     }
 
     private final ResourceLocation id;
+    @Nullable
     private final BiConsumer<PhosphophylliteCompound, IPayloadContext> clientCallback;
+    @Nullable
     private final BiConsumer<PhosphophylliteCompound, IPayloadContext> serverCallback;
     
-    public SimplePhosChannel(ResourceLocation id, BiConsumer<PhosphophylliteCompound, IPayloadContext> clientCallback, BiConsumer<PhosphophylliteCompound, IPayloadContext> serverCallback) {
+    public SimplePhosChannel(ResourceLocation id, @Nullable BiConsumer<PhosphophylliteCompound, IPayloadContext> clientCallback, @Nullable BiConsumer<PhosphophylliteCompound, IPayloadContext> serverCallback) {
         channelsToRegister.add(new Pair<>(id, this::handler));
         this.id = id;
         this.clientCallback = clientCallback;
@@ -43,8 +55,10 @@ public class SimplePhosChannel {
     
     private void handler(CompoundPacket compoundPacket, IPayloadContext ctx) {
         ctx.workHandler().execute(() -> {
-            var callbackToUse = ctx.flow().isClientbound() ? clientCallback : serverCallback;
-            callbackToUse.accept(compoundPacket.compound, ctx);
+            @Nullable final var callbackToUse = ctx.flow().isClientbound() ? clientCallback : serverCallback;
+            if (callbackToUse != null) {
+                callbackToUse.accept(compoundPacket.compound, ctx);
+            }
         });
     }
     
